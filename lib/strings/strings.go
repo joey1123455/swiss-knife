a87e85a5387bd68f4734f65f05d2a282d57a8b07
legionOfGo <folayanjoey@gmail.com>
2025-06-04 13:48:44 +0100

    feat: extract main domain

---
+++ lib/strings/strings.go
@@ -0,0 +1,14 @@
+package swissknife
+
+import (
+	"fmt"
+	"strings"
+)
+
+func ExtractMiddle(domain string) (string, error) {
+	parts := strings.Split(domain, ".")
+	if len(parts) < 3 {
+		return "", fmt.Errorf("domain must have at least 3 parts")
+	}
+	return parts[1], nil // the middle part
+}
